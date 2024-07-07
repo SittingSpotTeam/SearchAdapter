8b10b9e49ef27b536ccef9db93fbd754a97b1afe
Leonardo Malcotti <leo@Leonardos-Air.lan>
2024-07-07 14:30:36 +0200

    initialized spring app

---
+++ src/main/java/com/sittingspot/searchadapter/SearchadapterApplication.java
@@ -0,0 +1,13 @@
+package com.sittingspot.searchadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchadapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchadapterApplication.class, args);
+	}
+
+}
